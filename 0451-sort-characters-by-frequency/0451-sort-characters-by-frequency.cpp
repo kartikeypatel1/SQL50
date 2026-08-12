3340d44dab79a9ab5273761b318ac43ee5addb72
Kartikey Patel <kp0543985@gmail.com>
2026-08-13 02:27:12 +0530

    Time: 2 ms (62.15%), Space: 10.8 MB (88.66%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> mp;
+        priority_queue<pair<int, char>> pq;
+
+        for (char c : s)
+            mp[c]++;
+
+        for (auto it : mp)
+            pq.push({it.second, it.first});
+
+        string ans = "";
+
+        while (!pq.empty()) {
+            auto [freq, ch] = pq.top();
+            pq.pop();
+
+            while (freq--)
+                ans += ch;
+        }
+
+        return ans;
+    }
+};
