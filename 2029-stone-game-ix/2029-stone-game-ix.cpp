0109f2b9a929a7c75693d5e338b04cb36d79ab1d
Kartikey Patel <kp0543985@gmail.com>
2026-08-16 15:57:31 +0530

    Time: 7 ms (39.59%), Space: 131.3 MB (33.5%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt0=0,cnt2=0,cnt1=0;
+        for(int i:stones){
+            int x=i%3;
+            if(x==0) cnt0++;
+            else if(x==1) cnt1++;
+            else cnt2++;
+        }
+        if(cnt0%2==0){
+            return cnt1>=1&&cnt2>=1;
+        }
+        return abs(cnt1-cnt2)>2;
+    }
+};
